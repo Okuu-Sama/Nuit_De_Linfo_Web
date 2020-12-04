e0d0b8188ab4efcfa39cba84f7224373847837f7
StellaTham <stella.thammavong@gmail.com>
2020-12-04 05:43:39 +0100

    Team update

--- team.php
+++ team.php
@@ -1,11 +1,137 @@
 <!DOCTYPE html>
+<head>
 <meta charset="UTF-8">
-		<link rel="stylesheet" href="./css/team.css">
+        <link rel="stylesheet" href="./css/team.css">
+        <script src="./js/team.js"></script>
 		<title>Team Le Front Populaire</title>
-	</head>
+</head>
 <html>
-    <img src="./media/astolofo_plushes">
     <body>
-        Description à la con de l'équipe
+    <img id="crewmategif" src="./media/redrunning.gif">
+        <h1>Team members</h1>
+        <div class="our-team">
+        <div class="teampic">
+            <div class="profile-content">
+                <img src="./media/crewmate_nils.png" id="nilspic">
+                <div class="test-description">
+                    <h3>
+                        Nils CHOL<br>
+                        <small>Titre</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+
+            <div class="profile-content">
+                <img src="./media/crewmate_roman.png" id="romanpic">
+                <div class="test-description">
+                    <h3>
+                        Roman DIDELET<br>
+                        <small>Titre</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+
+            <div class="profile-content">
+                <img src="./media/crewmate_aurelien.png" id="aurelienpic">
+                <div class="test-description">
+                    <h3>
+                       Aurélien Andrieux<br>
+                        <small>Titre</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+
+            <div class="profile-content">
+                <img src="./media/crewmate_nassim.png" id="nassimpic">
+                <div class="test-description">
+                    <h3>
+                       Nassim El M'Rabti<br>
+                        <small>Titre</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+
+            <div class="profile-content">
+                <img src="./media/crewmate_stella.png" id="stellapic">
+                <div class="test-description">
+                    <h3>
+                       Stella Thammavong<br>
+                        <small>Titre</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+
+            <div class="profile-content">
+                <img src="./media/crewmate_nicolas.png" id="nicolaspic">
+                <div class="test-description">
+                    <h3>
+                       Nicolas Jatob<br>
+                        <small>Titre</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+
+            <div class="profile-content">
+                <img src="./media/crewmate_marine.png" id="marinepic">
+                <div class="test-description">
+                    <h3>
+                       Marine Elisabeth<br>
+                        <small>Titre</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+            
+            <div class="profile-content">
+                <img src="./media/crewmate_jason.png" id="jasonpic">
+                <div class="test-description">
+                     <h3>
+                      Jason<br>
+                        <small>Titre</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+
+            <div class="profile-content">
+                <img src="./media/crewmate_onurcan.png" id="onurcanpic">
+                <div class="test-description">
+                    <h3>
+                       Onur Can Kadioglu<br>
+                        <small>Titre</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+
+            <div class="profile-content">
+                <img src="./media/crewmate_karina.png" id="karinapic">
+                <div class="test-description">
+                    <h3>
+                       Karina Moussaoui<br>
+                        <small>Titre</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+
+            <div class="profile-content">
+                <img src="./media/crewmate_jeanlouis.png" id="jeanlouispic">
+                <div class="test-description">
+                    <h3>
+                       Jean-Louis Cheng<br>
+                        <small>Titre</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+        </div>
+    </div>
     </body>
 </html>
