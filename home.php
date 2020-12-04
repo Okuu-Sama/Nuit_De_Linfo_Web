99eb9183588a48a84073a71e22f3ee66405b9565
Nicolas <nicolasjatob@orange.fr>
2020-12-04 05:39:30 +0100

    Finished the home page

--- home.php
+++ home.php
@@ -6,17 +6,62 @@
 		initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="./css/home.css">
+        <script src="./js/home.js"></script>
         <title>Surfrider Web</title>
     </head>
     <body>
-        <nav id="menuBar" class="bar-container">
-            <li><img id="logo" src="./media/astolofo_plushes.png" alt=""></li>
+        <nav id="menuBar" class="bar-container flex-wraper">
+            <a href="home.php"><li><img id="logo" src="./media/astolfo_plushe.png" alt=""></li></a>
             <ul id="navMenu">
-                <li><a href="home.php"></a> Home</li>
-                <li><a href="home.php"></a> Story</li>
-                <li><a href="home.php"></a> The app</li>
-                <li><a href="home.php"></a> The team</li>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="home.php">Story</a></li>
+                <li><a href="home.php">The app</a></li>
+                <li><a href="team.php">The team</a></li>
             </ul>
         </nav>
+
+        <section>
+            <div id="presentation">
+                <p>Le surf fait rêver. Destinations paradisiaques, culture du cool, sport de nature en ont fait un vecteur
+d’image que les publicitaires adorent. Pourtant, telle une photo Instagram dont on découvrirait les
+coulisses, la pratique du surf (ou de toute autre activité nautique immergée) cache une réalité très
+différente du fantasme.</p><p>
+Otites, gastro-entérites, irritations cutanées, trois maladies qu’un tiers des surfeur·euse·s ont déjà
+affirmé avoir eu après une session de surf. Tout au long de l’année, les eaux dans lesquelles les
+surfeurs pratiquent leur sport favori sont soumises à des pollutions de deux types : bactériologiques
+et chimiques. On peut bien sûr rajouter les déchets. Ils envahissent les océans, sont une gêne
+visuelle et peuvent même être dangereux pour l’intégrité physique des pratiquants (la personne qui
+écrit ces lignes a littéralement failli se faire assommé par une cuvette de toilette !). </p><p>
+Pour agir contre ces risques, Surfrider souhaite que la surveillance des eaux littorales soit renforcée,
+et faite toute l’année. Aujourd’hui, seule une surveillance estivale est imposée au niveau européen.
+Pour appuyer nos revendications, nous avons besoin de données sur la pratique des surfeurs, leur
+fréquence, leur temps d’exposition et leurs observations sur le milieu. 
+                </p>
+            </div>
+            <div id="imgSurfer">
+                <img src=".\media\Surfer.png" alt="Surfer">
+            </div>
+        </section>
+
+        <p style="white-space: pre-wrap; min-height: 1.5em; text-align: left;"> </p>
+        
+        <section>
+            <div id="downloadtext">
+                <p>Join the community & Help your fellow surfers</p>
+            </div>
+
+            <div id="download">
+                <p>DOWNLOAD NOW</p>
+                <a href="home.php"><img src="./media/app_store_dl_button.png" alt=""></a>
+                <a href="home.php"><img id="gglplay" src="./media/google_play_btn.png" alt=""></a>
+            </div>
+            
+        </section>
+
+        <p style="white-space: pre-wrap; min-height: 1.5em; text-align: left;"> </p>
+        
+        <footer>
+            <p>NUIT DE L'INFO</p>
+        </footer>
     </body>
 </html>
--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
-test
+window.onload = function(){
+    var pres = document.getElementById("presentation");
+    var imgsurf = document.getElementById("imgSurfer");
+    
+    document.addEventListener('mousemove', move);
+    
+    function move(e)
+    {
+        posX = e.clientX / 100;
+        posX2= e.clientX / 100;
+        posY = e.clientY / 100;
+        posY2 = e.clientY / 100;
+        pres.style.transform = "translate(-" + posX + "px,-" + posY + "px)";
+        imgsurf.style.transform = "translate(" + posX2 + "px," +posY2 + "px)";
+    }
+}
