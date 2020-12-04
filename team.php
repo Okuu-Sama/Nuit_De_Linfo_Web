28505547dc7e7fa65dfdc4842ac8df5742600f43
StellaTham <stella.thammavong@gmail.com>
2020-12-04 01:14:40 +0100

    Team files

---
+++ team.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+		<link rel="stylesheet" href="./css/team.css">
+		<title>Team Le Front Populaire</title>
+	</head>
+<html>
+    <img src="./media/astolofo_plushes">
+    <body>
+        Description à la con de l'équipe
+    </body>
+</html>
