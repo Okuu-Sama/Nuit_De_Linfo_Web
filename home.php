dbb752574062972ebf38b110aae70c4336499be0
Nicolas <nicolasjatob@orange.fr>
2020-12-04 01:10:46 +0100

    added primitive header to home page

--- home.php
+++ home.php
@@ -1,6 +1,22 @@
 <!DOCTYPE html>
 <html>
+    <head>
+    <meta charset="UTF-8">
+		<met name="viewport" content="width=device-width,
+		initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="./css/home.css">
+        <title>Surfrider Web</title>
+    </head>
     <body>
-        
+        <nav id="menuBar" class="bar-container">
+            <li><img id="logo" src="./media/astolofo_plushes.png" alt=""></li>
+            <ul id="navMenu">
+                <li><a href="home.php"></a> Home</li>
+                <li><a href="home.php"></a> Story</li>
+                <li><a href="home.php"></a> The app</li>
+                <li><a href="home.php"></a> The team</li>
+            </ul>
+        </nav>
     </body>
 </html>
